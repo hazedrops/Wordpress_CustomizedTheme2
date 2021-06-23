a15c5c98a5c14c9cf3a96a9175d1819207b09414
hazedrops <hazedrops@hotmail.com>
2021-06-22 22:34:24 -0700

    WordPress Theme Development Practice Project

---
+++ page-templates/template-home.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Template Name: Template: Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<div class="home-banner">
+  <div class="container">
+    <h1>A Dining Experience</h1>
+  </div>
+</div>
+
+<div class="home-call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <a href="#" class="call-to-action">
+          <div class="image"></div>
+          <div class="title">Dining Menu</div>
+        </a>
+      </div>
+
+      <div class="col-md-4">
+        <a href="#" class="call-to-action second">
+          <div class="image"></div>
+          <div class="title">Lunch Menu</div>
+        </a>
+      </div>
+
+      <div class="col-md-4">
+        <a href="#" class="call-to-action third">
+          <div class="image"></div>
+          <div class="title">Functions</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="home-welcome">
+  <div class="container">
+    <h2>
+      <span>Welcome to</span> 
+      The Backbeach Eating House                        
+    </h2>
+  </div>
+</div>
+
+<?php
+get_footer();
+
+?>
